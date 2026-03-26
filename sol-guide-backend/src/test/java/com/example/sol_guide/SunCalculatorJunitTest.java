dd22571dec15f02b28b4c21ec528e2dd24fd34f0
madeleineMU24 <3tomu24.thunma@folkuniversitetet.nu>
2026-03-26 11:57:28 +0100

    fixat test som failade

--- sol-guide-backend/src/test/java/com/example/sol_guide/SunCalculatorJunitTest.java
+++ sol-guide-backend/src/test/java/com/example/sol_guide/SunCalculatorJunitTest.java
@@ -17,10 +17,10 @@ public class SunCalculatorJunitTest {
     @Test
     public void testIsHavingSun(){
 
-        Restaurant testRestaurant = new Restaurant(1L, "Test", 50.0, 60.0, 180, 180, false);
+        Restaurant testRestaurant = new Restaurant(1L, "Test", 59.3, 18.0, 180, 180, false);
 
         LocalDateTime time1 = LocalDateTime.of(2023, 6, 21, 13,30);
-        LocalDateTime time2 = LocalDateTime.of(2023, 6, 21, 21,30);
+        LocalDateTime time2 = LocalDateTime.of(2023, 6, 21, 23,30);
         boolean result1 = sunCalc.isHavingSun(testRestaurant, time1);
         boolean result2 = sunCalc.isHavingSun(testRestaurant, time2);
 
